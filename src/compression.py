dec2fc3576f79e0f20b220b181f120bd75dfde89
Andriy Kusyy <a.kusyy@ucu.edu.ua>
2018-02-15 21:13:37 +0200

    psnr

--- src/compression.py
+++ src/compression.py
@@ -1,10 +1,27 @@
-# -----------------first bucket-------------------
-import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy as np
+import pywt
+import scipy
 from numpy.linalg import svd
 from skimage import data, img_as_float
+from skimage import measure
 from skimage.color import rgb2gray
+
+
+def print_result_decorator(original_function):
+    def wrapper(*args, **kwargs):
+        res = original_function(*args, **kwargs)
+        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 5))
+        plt.gray()
+
+        ax[0].imshow(args[0])
+        ax[0].set_title('Original Image')
+
+        ax[1].imshow(res)
+        ax[1].set_title('Result')
+        plt.show()
+
+    return wrapper
 
 
 def perform_svd(matrix, k):
@@ -29,26 +46,12 @@ def perform_percent_svd(matrix, percent=0.9):
     U, s, V = svd(matrix)
     total_energy = np.sum(s)
     k = 1
-    while(np.sum(s[:k])/total_energy) <= percent:
+    while (np.sum(s[:k]) / total_energy) <= percent:
         k += 1
     return np.matrix(U[:, :k]) * np.diag(s[:k]) * np.matrix(V[:k, :])
 
-def print_result_decorator(original_function):
-    def wrapper(*args, **kwargs):
-        res = original_function(*args, **kwargs)
-        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 5))
-        plt.gray()
-    
-        ax[0].imshow(args[0])
-        ax[0].set_title('Original Image')
 
-        ax[1].imshow(res)
-        ax[1].set_title('Result')
-        plt.show()
-    return wrapper
-
-
-@print_result_decorator
+#@print_result_decorator
 def compress_image_with_svd(img_to_compress, k):
     """
     :param img_to_compress: 
@@ -59,21 +62,28 @@ def compress_image_with_svd(img_to_compress, k):
     compressed = perform_svd(img_matrix, k)
     return compressed
 
-img_matrix = img_as_float(rgb2gray(data.astronaut()))
-svd_compressed = compress_image_with_svd(data.astronaut(), 50)
-l = perform_percent_svd(img_matrix)
-    
-    
-# -----------------------------second bucket -------------------------
-import pywt
-import scipy
 
-@print_result_decorator
+#@print_result_decorator
 def perform_dwt(image_to_compress):
-    scipy.misc.imsave('photo/real.jpg', image_to_compress)
-    coefs = pywt.dwt2(perform_svd(image_to_compress,50), 'haar')
-    compressed, (a,b,c) = coefs
-    scipy.misc.imsave('photo/compressed.jpg', compressed)
+    #scipy.misc.imsave('photo/real.jpg', image_to_compress)
+    coefs = pywt.dwt2(image_to_compress, 'haar')
+    compressed, (a, b, c) = coefs
+    #scipy.misc.imsave('photo/compressed.jpg', compressed)
     return compressed
-    
-perform_dwt(img_matrix)
+
+
+if __name__ == '__main__':
+    img_matrix = img_as_float(rgb2gray(data.astronaut()))
+    # svd_compressed = compress_image_with_svd(data.astronaut(), 50)
+
+    svd_compressed = perform_percent_svd(img_matrix)
+
+    fully_compressed = perform_dwt(svd_compressed)
+
+    svd_compression_ratio = measure.compare_psnr(img_matrix, svd_compressed)
+    dwt_compression_ratio = measure.compare_psnr(svd_compressed, fully_compressed)
+    total_compression_ratio = measure.compare_psnr(img_matrix, fully_compressed)
+
+    print(svd_compression_ratio)
+    print(dwt_compression_ratio)
+    print(total_compression_ratio)
